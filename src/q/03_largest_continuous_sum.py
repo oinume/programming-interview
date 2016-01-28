6b84a1422e633c2af523624a71810ff6ff06ce0c
Kazuhiro Oinuma <oinume@gmail.com>
2016-01-28 21:00:49 +0900

    largest continuous sum

---
+++ src/q/03_largest_continuous_sum.py
@@ -0,0 +1,14 @@
+# http://www.ardendertat.com/2011/09/24/programming-interview-questions-3-largest-continuous-sum/
+
+def largestContinuousSum(array):
+    if len(array) == 0:
+        return 0
+    maxSum = currentSum = array[0]
+    for num in array[1:]:
+        currentSum = max(currentSum+num, num)
+        maxSum = max(currentSum, maxSum)
+    return maxSum
+
+if __name__ == '__main__':
+    print(largestContinuousSum([100, 6, -46, 10, 100])) # 106
+    print(largestContinuousSum([100, 6, -46, 10, 10])) # 170
